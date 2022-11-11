61e9317d8e380e66d9560eb946facc8ae3d88a26
emreyilmaz1912 <117239489+emreyilmaz1912@users.noreply.github.com>
2022-11-11 15:45:14 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Etkinlik önermesi almak için aşağıya sıcak değerini giriniz.");
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Sıcaklık değerini giriniz :");
+            heat = input.nextInt();
+
+            if (heat < 5) {
+                System.out.println("Kayak yapabilirsiniz." );
+
+            } else if (heat <= 25) {
+                if (heat <= 15) {
+                    System.out.println("Sinemaya gidebilirsiniz.");
+                }
+                if (heat >= 10) {
+                    System.out.println("Pikniğe gidebilirsiniz.");
+                }
+            } else {
+                System.out.println("Yüzmeye gidebilirsiniz.");
+            }
+        }
+    }
+}
